cf4c6afc91f8889f4ef0cb3c256825f9554a91c1
ABHISHEK C R <imabhishekcr@gmail.com>
2024-12-29 09:02:46 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://https://adafc46add92a4abe8a2fc5f962a5b47-1112532817.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://adafc46add92a4abe8a2fc5f962a5b47-1112532817.ap-south-1.elb.amazonaws.com',
 };
